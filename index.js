a7fe797ef84ccfd8c78fc2ef4d8f83640943120e
aliceduren19 <aliceduren19@gmail.com>
2021-12-17 18:46:14 -0800

    ad

---
+++ index.js
@@ -0,0 +1,20 @@
+
+//Wednesday, 24 July 2020
+const dayNames=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+let dayObj= new Date()
+let weekday=dayNames[dayObj.getDay()];
+document.getElementById('day').textContent=weekday;
+
+let dayn=new Date()
+let daynum=dayn.getDate()
+document.getElementById("daynum").textContent=daynum
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
+let m=new Date()
+let n=monthNames[m.getMonth()];
+document.getElementById("month").textContent=n
+
+let x=new Date()
+let y=x.getFullYear()
+document.getElementById("year").textContent=y
